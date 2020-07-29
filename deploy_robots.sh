16d2eb5a5b840483ee166d43443c333dd159566d
Bharathi ponnusamy <bharathi@endpoint.com>
2020-07-30 05:19:24 +0530

    initial commit

---
+++ deploy_robots.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#set -x
+file=test.json
+
+for CLIENT in $(jq ". | keys | .[]" $file); do
+    update=$(jq -r ".[$CLIENT].update" $file);
+    if [ $update == true ]; then
+      host=$(jq -r ".[$CLIENT].host" $file);
+      sites=$(jq -r ".[$CLIENT].site" $file);
+      for path in $(jq -r ".[$CLIENT].site | values | .[]" $file); do
+        ansible-playbook manage_robots.yml -i server.ini --extra-vars '{"host":'"$host"',"path":'"$path"'}'
+      done
+    fi
+done
+
+#ansible-playbook manage_robots.yml -i server.ini --extra-vars '{"host":"ln1","path":"test"}'
+
+#----------------------------------------------------
+## BEGIN ANSIBLE MANAGED BLOCK
+
+## END ANSIBLE MANAGED BLOCK
+#----------------------------------------------------
+#Example Block
+#----------------------------------------------------
+## BEGIN ANSIBLE MANAGED BLOCK
+#User-Agent: Applebot
+#Disallow: /
+#
+#User-Agent: MegaIndex
+#Disallow: /
+## END ANSIBLE MANAGED BLOCK
+#----------------------------------------------------
